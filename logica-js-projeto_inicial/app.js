ea43929e8319d505bcccedd6b062d64c89daca09
heitorv-cloud <heitorv500@gmail.com>
2026-03-11 00:14:17 -0300

    primeiro commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,42 @@
+/*alert('boas vindas ao jogo do mundo secreto');
+let numeroSecreto= 5;
+let chute = prompt('escolha um numero entre 1 a 10');
+
+if(chute == numeroSecreto) {
+    console.log('isso ai! vc descobriu o numero secreto (5)');
+} 
+
+
+
+let mensagem = 'bem vindo';
+alert(mensagem);
+let resposta = prompt (voce é cadastrado?);
+let cadastrado= faça login;
+let naocadastrado= faça o cadastro;
+
+if (resposta === 'sim') {
+    alert(cadastrado);
+} else{
+    alert(naocadastrado)
+}*/
+
+//desafios logica em programação 
+
+//alert ('Boas vindas ao nosso site!');
+
+/*let nome = lua;
+let idade = 25;
+let numerodevendas = 50;
+let saldodisponivel = 1000;
+*/
+
+/*let mensagemdeerro = 'erro! Preencha todos os campos';
+alert(mensagemdeerro);
+*/
+
+let mensagemparabens = 'vc passou no teste, parabens!';
+alert(mensagemparabens);
+console.log(mensagemparabens);
+
+
+ 
